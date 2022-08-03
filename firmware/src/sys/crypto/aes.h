d32a6c34e6e28a06570ed20bd0e6c6d11b749b48
takayama <takayama@foxmail.com>
2022-08-03 20:38:49 +0900

    add aes 256 cbc

--- firmware/src/sys/crypto/aes.h
+++ firmware/src/sys/crypto/aes.h
@@ -1,13 +1,25 @@
 #ifndef __SYS_CRYPTO_AES_H
 #define __SYS_CRYPTO_AES_H
 
-#include <aes/target/CM0.h>
+#include <stdint.h>
+#include <stddef.h>
 
-typedef 
+#define AES128_KEY_ROUNDS 10
+#define AES192_KEY_ROUNDS 12
+#define AES256_KEY_ROUNDS 14
 
+/**
+ * @param key length = 32 bytes (256 bits)
+ * @param iv length = 16 bytes
+ * @param len data length, must be a multiple of 16
+ */
+void crypto_aes256cbc_encrypt(uint8_t *key, uint8_t *iv, uint8_t *data_in, uint8_t *data_out, size_t len);
 
-void crypto_aes_init(uint8_t *roundkey, )
-
-
+/**
+ * @param key length = 32 bytes (256 bits)
+ * @param iv length = 16 bytes
+ * @param len data length, must be a multiple of 16
+ */
+void crypto_aes256cbc_decrypt(uint8_t *key, uint8_t *iv, uint8_t *data_in, uint8_t *data_out, size_t len);
 
 #endif /* __SYS_CRYPTO_AES_H */
